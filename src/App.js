a59d0b4ca1b03bcce587d6050123167d734b427c
Will McGill <mcgill.will@yahoo.com>
2019-10-23 10:07:40 -0400

    built menu navigation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Jumbotron_BS from './Jumbotron_BS';
 import MenuNav from './MenuNav';
+import DynamicTable from './CreateTable'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
@@ -12,22 +13,23 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = [
-      { name: "Breakfast", Menu: "" },
-      { name: "Lunch", Menu: "" },
-      { name: "Dinner", Menu: "" },
-      { name: "Sides", Menu: "" },
-      { name: "Dessert", Menu: "" }
+      { name: "Breakfast"},
+      { name: "Lunch"},
+      { name: "Dinner"},
+      { name: "Sides"},
+      { name: "Dessert"}
     ]
   }
 
   componentDidMount() {
-
+    
     // Check if menu data already exists in local storage.
     //If so, store data in local storage in menu state, else pull from API
 
     if (localStorage.getItem('Breakfast')) {
       this.state.map(MenuItems_Local => {
-        this.state.Menu = JSON.parse(localStorage.getItem(MenuItems_Local.name));
+      JSON.parse(localStorage.getItem(MenuItems_Local.name));
+        
       })
     }
     else {
@@ -35,18 +37,20 @@ class App extends React.Component {
         axios.get('https://entree-f18.herokuapp.com/v1/menu/12')
           .then(items => {
             localStorage.setItem(MenuItems.name, JSON.stringify(items.data.menu_items))
-            this.state.Menu = items.data.menu_items;
-          })
+            })
           ;
       })
     }
+    console.log(this.state)
   }
-
+  
   render() {
     return (
       <div className="App">
         <Jumbotron_BS title="Sub-Standard" description="The standard in sub sammichs" />
         <MenuNav />
+        <DynamicTable MealName = {this.state}/>
+      
       </div>
     )
   };
--- src/MenuNav.js
+++ src/MenuNav.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Tabs, Tab } from 'react-bootstrap-tabs';
 
-
-
 class MenuNavBar extends React.Component {
     render() {
         return (
             <Tabs onSelect={(index, label) => console.log(label + ' selected')}>
-                <Tab label="Tab1">Tab 1 content</Tab>
-                <Tab label="Tab2">Tab 2 content</Tab>
+
+
+                <Tab label="Breakfast">Tab 1 content</Tab>
+                <Tab label="Lunch">Tab 2 content</Tab>
+                <Tab label="Dinner">Tab 1 content</Tab>
+                <Tab label="Sides">Tab 2 content</Tab>
+                <Tab label="Dessert">Tab 1 content</Tab>
+
+
             </Tabs>
+
         )
 
     }
