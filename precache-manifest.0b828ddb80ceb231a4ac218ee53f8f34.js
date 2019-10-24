dd4d2835b037ba6f8359e853640dc18791346c1b
Will McGill <mcgill.will@yahoo.com>
2019-10-24 16:15:28 -0400

    Updates

---
+++ precache-manifest.0b828ddb80ceb231a4ac218ee53f8f34.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3974e4daa04130e1dc061b8b67040f3e",
+    "url": "/ReactRestaurant/index.html"
+  },
+  {
+    "revision": "fec63a05d6813a7cc761",
+    "url": "/ReactRestaurant/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "6afce4c7a7ef5bb4d919",
+    "url": "/ReactRestaurant/static/css/main.90cf97b7.chunk.css"
+  },
+  {
+    "revision": "fec63a05d6813a7cc761",
+    "url": "/ReactRestaurant/static/js/2.b900e4a1.chunk.js"
+  },
+  {
+    "revision": "6afce4c7a7ef5bb4d919",
+    "url": "/ReactRestaurant/static/js/main.a6237eb8.chunk.js"
+  },
+  {
+    "revision": "fb422cf04f1220f26032",
+    "url": "/ReactRestaurant/static/js/runtime-main.35bf8d5e.js"
+  },
+  {
+    "revision": "19543b1d59d3765fc4619c8a7b2dcfba",
+    "url": "/ReactRestaurant/static/media/bread-bun-chopping-board-tall.19543b1d.jpg"
+  },
+  {
+    "revision": "994932a666296f83cddd03c804d64736",
+    "url": "/ReactRestaurant/static/media/bread-bun.994932a6.jpeg"
+  }
+]);
